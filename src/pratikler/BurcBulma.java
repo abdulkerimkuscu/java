7d778dea93bc8127fc1f786761b896ee032bcc68
Abdülkerim <kuscu2708@gmail.com>
2023-05-07 17:30:33 +0300

    Pratikler

--- src/pratikler/BurcBulma.java
+++ src/pratikler/BurcBulma.java
@@ -11,31 +11,31 @@ public class BurcBulma {
         System.out.print("Doğduğunuz Gün (1-31): ");
         day = input.nextInt();
 
-        if((month == 1 && day >= 22) || (month == 2 && day<=19)){
+        if ((month == 1 && day >= 22) || (month == 2 && day <= 19)) {
             System.out.println("Burcunuz Kova");
-        } else if ((month == 2 && day>= 20) || (month == 3 && day<=20)) {
+        } else if ((month == 2 && day >= 20) || (month == 3 && day <= 20)) {
             System.out.println("Burcunuz Balık");
-        }else if ((month == 3 && day>= 21) || (month == 4 && day<=20)) {
+        } else if ((month == 3 && day >= 21) || (month == 4 && day <= 20)) {
             System.out.println("Burcunuz Koç");
-        }else if ((month == 4 && day>= 21) || (month == 5 && day<=21)) {
+        } else if ((month == 4 && day >= 21) || (month == 5 && day <= 21)) {
             System.out.println("Burcunuz Boğa");
-        }else if ((month == 5 && day>= 22) || (month == 6 && day<=22)) {
+        } else if ((month == 5 && day >= 22) || (month == 6 && day <= 22)) {
             System.out.println("Burcunuz İkizler");
-        }else if ((month == 6 && day>= 23) || (month == 7 && day<=22)) {
+        } else if ((month == 6 && day >= 23) || (month == 7 && day <= 22)) {
             System.out.println("Burcunuz Yengeç");
-        }else if ((month == 7 && day>= 23) || (month == 8 && day<=22)) {
+        } else if ((month == 7 && day >= 23) || (month == 8 && day <= 22)) {
             System.out.println("Burcunuz Aslan");
-        }else if ((month == 8 && day>= 23) || (month == 9 && day<=22)) {
+        } else if ((month == 8 && day >= 23) || (month == 9 && day <= 22)) {
             System.out.println("Burcunuz Başak");
-        }else if ((month == 9 && day>= 23) || (month == 10 && day<=22)) {
+        } else if ((month == 9 && day >= 23) || (month == 10 && day <= 22)) {
             System.out.println("Burcunuz Terazi");
-        }else if ((month == 10 && day>= 23) || (month == 11 && day<=21)) {
+        } else if ((month == 10 && day >= 23) || (month == 11 && day <= 21)) {
             System.out.println("Burcunuz Akrep");
-        }else if ((month == 11 && day>= 22) || (month == 12 && day<=21)) {
+        } else if ((month == 11 && day >= 22) || (month == 12 && day <= 21)) {
             System.out.println("Burcunuz Yay");
-        }else if ((month == 12 && day>= 22) || (month == 1 && day<=21)) {
+        } else if ((month == 12 && day >= 22) || (month == 1 && day <= 21)) {
             System.out.println("Burcunuz Oğlak");
-        }else {
+        } else {
             System.out.println("Hatalı Giriş Yaptınız !!!");
         }
     }
