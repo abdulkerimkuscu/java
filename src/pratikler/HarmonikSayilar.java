d7ed453859c84d1d3c7818983b84d02eb795fe93
Abdülkerim <kuscu2708@gmail.com>
2023-05-08 21:27:38 +0300

    Pratikler

---
+++ src/pratikler/HarmonikSayilar.java
@@ -0,0 +1,18 @@
+package pratikler;
+
+import java.util.Scanner;
+
+public class HarmonikSayilar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("N sayısını giriniz: ");
+        int n = input.nextInt();
+
+        double result = 0.0;
+
+        for(double i = 1; i<=n; i++){
+            result += (1.0/i);
+        }
+        System.out.println(result);
+    }
+}
