3d835895bc4ff6a4ed251db39d46e89b90a5d005
Abdülkerim <kuscu2708@gmail.com>
2023-05-11 21:11:01 +0300

    Pratikler

---
+++ src/odevler/ArtikYil.java
@@ -0,0 +1,25 @@
+package odevler;
+
+import java.util.Scanner;
+
+public class ArtikYil {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Yıl Giriniz: ");
+        int year = input.nextInt();
+
+        //Genel bir kural olarak, artık yıllar 4 rakamının katı olan yıllardır
+        //Ancak 100'ün katı olan yıllardan sadece 400'e kalansız olarak bölünebilenler artık yıldır
+        //Bu yüzden yılın 4 e bölümünden kalan 0 olmalı veya 400 e bölümünden kalan 0 olmalı
+        /*Ancak 4 e ve 100 e bölünüp 400 e bölünememesi için 100 e bölmünden kalanın 0 a eşit olmaması gerek
+        Bu sebebple ilk koşul sağlanamadığı için false dönmeli ki hatalı sonuç almamalıyız.
+        Biraz karışık gibi gözüksede kodları incelerseniz dediğim anlaşılır :))))))
+         */
+
+        if ((year % 4 == 0 && year % 100 !=0) || (year % 400 == 0)){
+            System.out.println(year +"Artık Yıldır");
+        }else {
+            System.out.println(year + "Artık Yıl Değildir");
+        }
+    }
+}
